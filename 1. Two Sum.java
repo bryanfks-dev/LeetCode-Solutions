26b2bc59cea483000cfc43cb783d29e7cdfc530f
Bryan Fernando <bryanfernandoks11@gmail.com>
2023-09-20 21:48:38 +0700

    commit

--- 1. Two Sum.java
+++ 1. Two Sum.java
@@ -1,14 +1,20 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        // Create hash map
         Map<Integer, Integer> map = new HashMap();
 
+        // Itterate through array element
         for (int idx = 0; idx < nums.length; idx++) {
+            // Complement of target and current element
             int complement = target - nums[idx];
 
+            // Check if complement exists in map
             if (map.containsKey(complement)) {
-                return new int[]{map.get(complement), idx};
+                // Return results
+                return new int[] { map.get(complement), idx };
             }
-
+            
+            // Put element into map
             map.put(nums[idx], idx);
         }
 
