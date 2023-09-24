dc9c853425d6f15f5a05241bd5affb3e5f7254f7
Bryan Fernando <bryanfernandoks11@gmail.com>
2023-09-25 01:45:55 +0700

    commit

---
+++ 2806. Account Balance After Rounded Purchase.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int accountBalanceAfterPurchase(int amt) {
+        // Base case
+        if (amt % 10 == 0) {
+            return 100-amt;
+        }
+
+        int ans = 100-amt;
+
+        return (amt % 10 >= 5) ? (ans / 10) * 10 : ((ans / 10) + 1) * 10;
+    }
+}
